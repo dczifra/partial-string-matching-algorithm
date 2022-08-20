4399bda7e2410a2f2cd54e75959bbb12387cbcbd
Jason melman <jasonmelman@pop-os.localdomain>
2022-08-20 16:19:58 -0700

    Cleaned up string_match script file

--- string_search/string_match.py
+++ string_search/string_match.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import time
-
 def string_match(full_vals, full_vals_col_name , matching_phase):
     """string_match
 
@@ -21,23 +18,3 @@ def string_match(full_vals, full_vals_col_name , matching_phase):
     full_vals['match_phrase'] = matching_phase # add matching phrase to the dataframe
     
     return full_vals
-    
-
-# input = pd.read_csv('test_files/3q_semrush.csv')
-# match = pd.read_csv('test_files/3q_test_bigrams.csv')
-
-
-# # start timer
-# start_time = time.time()
-
-# # list comprehension on string_match() to check for matches in input dataframe vs each row in match dataframe
-# matching_phases = [string_match(input, 'Keyword', i) for i in match['match']]
-
-# df = pd.concat(matching_phases)
-
-# # print where matches are found
-# match_exist = df[df['match_exist'] == True]
-# print(match_exist[['Keyword', 'match_phrase']])
-
-# end_time = time.time()
-# print("Time elapsed: {} seconds".format(end_time - start_time))
